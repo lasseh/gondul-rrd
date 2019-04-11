f70c7723925434a5ee88aaaf8dd3b6de589f4f60
Lasse Haugen <haugen.lasse@gmail.com>
2019-04-11 11:00:29 +0200

    Added /switches list from database and theme support

--- api/graph.go
+++ api/graph.go
@@ -11,10 +11,6 @@ import (
 	"github.com/ziutek/rrd"
 )
 
-const (
-	rrdPath = "../rrd/"
-)
-
 func graphHandler(c *gin.Context) {
 	rrdStart := c.DefaultQuery("start", "-7200s")
 	rrdEnd := c.DefaultQuery("end", "-60s")
@@ -24,8 +20,7 @@ func graphHandler(c *gin.Context) {
 	rrdHeight := c.DefaultQuery("height", "200")
 	rrdDevice := c.Query("device")
 	rrdInterface := c.Query("interface")
-	rrdNight := c.Query("night")
-	rrdUgly := c.Query("ugly")
+	rrdTheme := c.Query("theme")
 
 	if len(rrdDevice) == 0 {
 		//TODO Return a rrd image with "no data found"
@@ -39,7 +34,7 @@ func graphHandler(c *gin.Context) {
 	}
 	// Prettify interface names
 	iface := strings.Replace(rrdInterface, "/", "", -1)
-	filename := fmt.Sprintf("%s%s/%s.rrd", rrdPath, rrdDevice, iface)
+	filename := fmt.Sprintf("%s%s/%s.rrd", rrdLol, rrdDevice, iface)
 
 	fmt.Println("Filename:", filename)
 
@@ -59,21 +54,33 @@ func graphHandler(c *gin.Context) {
 	g.SetSlopeMode()
 	g.SetRigid()
 	g.SetAltAutoscaleMax()
-	g.SetWatermark("Nora <3")
+	//g.SetWatermark("Nora <3")
 
 	if rrdLegend == "0" {
 		g.SetNoLegend()
 	}
 	// Nightmode
 	// not finished
-	if rrdNight == "1" {
-		g.SetColor("BACK", "343434FF")
-		g.SetColor("FONT", "FFFFFFFF")
-		g.SetColor("CANVAS", "313131FF")
-		g.SetColor("SHADEA", "343434FF")
-		g.SetColor("SHADEB", "343434FF")
-		g.SetColor("GRID", "313131FF")
-		g.SetColor("MGRID", "292929FF")
+	if rrdTheme == "dark" {
+		g.SetColor("BACK", "31313DFF")
+		g.SetColor("FONT", "bec2ccFF")
+		g.SetColor("CANVAS", "31313DFF")
+		g.SetColor("SHADEA", "31313DFF")
+		g.SetColor("SHADEB", "31313DFF")
+		g.SetColor("GRID", "31313DFF")
+		g.SetColor("MGRID", "FF7E2730")
+		g.SetColor("AXIS", "31313DFF")
+		// g.SetColor("FRAME", "999999FF")
+		g.SetColor("ARROW", "A500A5FF")
+	}
+	if rrdTheme == "old" {
+		//g.SetColor("BACK", "141474FF")
+		//g.SetColor("FONT", "F3FF3FFF")
+		//g.SetColor("CANVAS", "313131FF")
+		//g.SetColor("SHADEA", "343834FF")
+		//g.SetColor("SHADEB", "343434FF")
+		//g.SetColor("GRID", "313231FF")
+		//g.SetColor("MGRID", "292929FF")
 		//g.SetColor("AXIS", "999999FF")
 		//g.SetColor("FRAME", "999999FF")
 		//g.SetColor("ARROW", "999999FF")
@@ -85,8 +92,16 @@ func graphHandler(c *gin.Context) {
 	g.CDef("cdefd", "b,8,*")
 	g.CDef("cdefe", "c,8,*")
 	// Inbound
-	g.Area("cdefd", "00CF0050", "")
-	g.Line(1, "cdefd", "00CF00FF", "Inbound\t")
+	if rrdTheme == "dark" {
+		g.Area("cdefd", "A500A550", "")
+		g.Line(2, "cdefd", "A500A5FF", "Inbound\t")
+	} else if rrdTheme == "old" {
+		g.Area("cdefd", "00CF0050", "")
+		g.Line(1, "cdefd", "00CF00FF", "Inbound\t")
+	} else {
+		g.Area("cdefd", "00CF0050", "")
+		g.Line(1, "cdefd", "00CF00FF", "Inbound\t")
+	}
 	g.VDef("c_in", "cdefd,LAST")
 	g.VDef("a_in", "cdefd,AVERAGE")
 	g.VDef("m_in", "cdefd,MAXIMUM")
@@ -94,8 +109,15 @@ func graphHandler(c *gin.Context) {
 	g.GPrint("a_in", "Average\\:%8.2lf%s\t")
 	g.GPrint("m_in", "Maximum\\:%8.2lf%s\\n")
 	// Outbound
-	g.Area("cdefe", "002A9750", "")
-	g.Line(1, "cdefe", "002A97FF", "Outbound\t")
+	if rrdTheme == "dark" {
+		g.Area("cdefe", "31c0f640", "")
+		g.Line(2, "cdefe", "31c0f6FF", "Outbound\t")
+	} else if rrdTheme == "old" {
+		g.Line(1, "cdefe", "212125FF", "Outbound\t")
+	} else {
+		g.Area("cdefe", "002A9750", "")
+		g.Line(1, "cdefe", "002A97FF", "Outbound\t")
+	}
 	g.VDef("c_out", "cdefe,LAST")
 	g.VDef("a_out", "cdefe,AVERAGE")
 	g.VDef("m_out", "cdefe,MAXIMUM")
--- api/main.go
+++ api/main.go
@@ -1,12 +1,71 @@
 package main
 
 import (
+	"flag"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-const gondulURL = "https://gondul.lasse.cloud"
+var rrdLol = ""
+var dbLol = ""
+var db *gorm.DB
+var err error
+
+// Device is the sqlite structure
+type Device struct {
+	Timestamp     time.Time `json:"timestamp"`
+	Device        string    `json:"device"`
+	Interface     string    `json:"interface"`
+	IfHCInOctets  uint64    `json:"ifHCInOctets"`
+	IfHCOutOctets uint64    `json:"ifHCOutOctets"`
+	IfAlias       string    `json:"ifAlias"`
+}
+
+// Datalol is ..
+type Datalol struct {
+	Switches map[string]*Switch `json:"switches"`
+}
+
+// Switch is
+type Switch struct {
+	Ifs    map[string]*IfStat `json:"ifs" db:"interface"`
+	Totals *Total             `json:"totals"`
+}
+
+// IfStat is
+type IfStat struct {
+	Timestamp     time.Time `json:"timestamp"`
+	IfHCInOctets  uint64    `json:"ifHCInOctets"`
+	IfHCOutOctets uint64    `json:"ifHCOutOctets"`
+	IfAlias       string    `json:"ifAlias"`
+}
+
+// Total is ...
+type Total struct {
+	IfHCInOctets  uint64 `json:"ifHCInOctets"`
+	IfHCOutOctets uint64 `json:"ifHCOutOctets"`
+	Total         uint64 `json:"total"`
+	Live          uint64 `json:"live"`
+}
 
 func main() {
+	// Flags
+	rrdPath := flag.String("path", "rrd/", "Path to rrd files")
+	dbPath := flag.String("db", "/var/lib/gondul-rrd.sqlite", "Path to database")
+	flag.Parse()
+	rrdLol = *rrdPath
+	dbLol = *dbPath
+
+	// Database
+	db, err = gorm.Open("sqlite3", *dbPath)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+	db.LogMode(true)
 
 	gin.SetMode(gin.DebugMode)
 
@@ -18,10 +77,66 @@ func main() {
 	r.GET("/graph", graphHandler)
 
 	// Serve the interface tree list created by the collector
-	// r.GET("/tree", treeHandler)
+	r.GET("/switches", switchHandler)
 
-	// Web
-	r.Static("/", "../web")
+	// Index
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"switches": "http://rrd.lasse.cloud/switches",
+			"graph":    "https://rrd.lasse.cloud/graph?width=1700&height=600&legend=1&start=-24h&end=-1m&device=r1.tele&interface=ae0&title=Interwebz&theme=dark",
+		})
+	})
 
 	r.Run(":8080")
+}
+
+func switchHandler(c *gin.Context) {
+	var d []Device
+	sw := NewDatalol()
+	if err := db.Table("devices").Find(&d); err != nil {
+		c.AbortWithStatus(404)
+	}
+
+	for _, v := range d {
+		if _, ok := sw.Switches[v.Device]; !ok {
+			sw.Switches[v.Device] = NewSwitch()
+		}
+		sw.Switches[v.Device].Ifs[v.Interface] = v.GetIfStat()
+	}
+
+	for _, v := range sw.Switches {
+		t := &Total{}
+		v.Totals = t
+		for _, values := range v.Ifs {
+			t.Total++
+			t.IfHCOutOctets += values.IfHCOutOctets
+			t.IfHCInOctets += values.IfHCInOctets
+		}
+	}
+
+	c.JSON(200, sw)
+}
+
+// GetIfStat populates ..
+func (d *Device) GetIfStat() *IfStat {
+	return &IfStat{
+		Timestamp:     d.Timestamp,
+		IfHCInOctets:  d.IfHCInOctets,
+		IfHCOutOctets: d.IfHCOutOctets,
+		IfAlias:       d.IfAlias,
+	}
+}
+
+// NewSwitch creates a new switch
+func NewSwitch() *Switch {
+	var s Switch
+	s.Ifs = make(map[string]*IfStat)
+	return &s
+}
+
+// NewDatalol is ..
+func NewDatalol() *Datalol {
+	var d Datalol
+	d.Switches = make(map[string]*Switch)
+	return &d
 }
